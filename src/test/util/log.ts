b298c8f81d91eaa25e78a1cf31d3e3f89adbd091
gaoxiao6331 <gaoxiao6331@163.com>
2025-01-01 00:09:27 +0800

    feat: add logger

--- src/test/util/log.ts
+++ src/test/util/log.ts
@@ -1,9 +1,5 @@
 import {defaultLogger} from "../../util/log";
-import chalk from "chalk";
 
 defaultLogger.error("this is an error test");
 defaultLogger.info("this is an info test");
 defaultLogger.warn("this is an warn test");
-
-const a= chalk.red("11")
-console.log(a)
